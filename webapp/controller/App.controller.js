66935b263385ce2ffa6e8611f7e881e3ecc72af0
Hemanth0711 <hemanthsai0711@gmail.com>
2024-05-07 09:46:01 +0000

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.mindset.assessmentapp1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
